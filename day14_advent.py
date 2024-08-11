f4a88b03d28b3a0d46e7a6b391b0911c6ee92428
Marcin Swierczewski <>
2024-08-11 18:18:23 +0100

    reformat

--- day14_advent.py
+++ day14_advent.py
@@ -1,230 +1,115 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Dec 25 17:20:10 2023
-
-@author: marcinswierczewski
-"""
-
-BOARD = """#....O#O.......O.......#O.......#...#O#.O...O...O..O#..#.O......OOO.OOO...#O.O.........OO.#..O....O.
-##.#O..........OO.#.#..#...O..O.O##....O.....#....O.#....O....O.#..OOO..#..O.OO..##.#O#OO..##OO..#..
-#.O.#O.O.O.......#.....##.##......#.###..###........OO...#..OO.....O....O......OOO.OOO.......OO.O#.O
-O#.....#......#..##O#O.#..#O#.O.O.#...O....O.#......#....#.....O.#........O....O...O..O#...O...#O...
-O#..#.OO.O#..#O##.#.#..O...OO..#..O#...#.....O.#.OO#...O.OO.O.O.....#...#.O..##...O.#.O.....O.##..O#
-..OOO...#O#...O...OO.#...#OO.....#O..#.#.#OO...O.O#.......#..O..O..##..O#.O.....#O..OO#......O.O.O.O
-..O.......O.....##OO#OO.#..#...O.O...O...O...#OOOO.O.O..#..O....O....OO#O.O.OOOO...#OO#.OO.O.#...#.#
-#..O.......O.O...O.....#.O...O.#.....##.#...#.#.O...O...#.O.....O....#........#.O.#.#O#......O#..O.O
-..#.#OO..#..OO.#..O.O.OO....OOOO...OO#..O..O...OO.O..O.O...#..#.....#.......O...O.......##O#....#.O#
-.O.O#..O..O.#OO..O...#.O..O##O..##OO.OO.O.OO.O#...O...O........#.O..#......O#O#.O........OO..O......
-.O#...........O..#...O.O......#O..###O....#O...OO...........#OO....O..OO..O..O.#O..O......#.O.O..#..
-O..O#....OO....#.##.O#....OO.......O.......O....##.O.O....O#...#.O.O###...#....#..#...#...#OO....O##
-###.O..#O..#O.#.#......##...OO...#.....OO.#O#..#...#..##O..#...O#....#O#...O..O...O...O..O.O..O..#.O
-OOO.#.#..O..O#.O....#...O.OO###.#........OO.#.O#.#.....O..O..O#...O.O.O#.#O......O.#OOO#.#O.#O.O..OO
-...##O#..O.#O......OO.#..OO....OOOO#...O...O.#....#..O...........OOO#.O.OO..O..O...O.#..O..OO##.O#..
-.#.....#..#.....#..#OOO.O#O..O#O#..O.....OO..O##.O.OOO...#O....#O...O#...#O..O#O##....#.....O.O..O.#
-OO............O....O##..OO.#..O.O.....#O...#.##O.#.O.OO#....#...O..O.#.##...........##....O.OO..O...
-.##O#OO......O#...OO#...##O.O.......O....OOO..O..#.#...OOO.#..#.#..O..O#..O.....#.#.OO...O.....O##.O
-O..#OOO#.O...O.......O#....O.O...O......O.......OO....##......OO#.....##...O#.............O#.....O#.
-..O..##OO..O...#..OO..#...#.O.O..#..O...O.###..O.......#.O....O.##....OO.#..O.......#.O.O...O.O.####
-.....O..OOO##..#.........O......O..O.#O##O#..O#O......##..O.O..#.......#.O##.#..#.#....O.OO.O.#O....
-##....O..#.....#O.O...O#O#O.#.....O#.O.#..#O..O...O.#.#.....O.......#O...............#O......OO#..#.
-..#O....#.......O..#O..#.OO..O...##.O.....O.#....#O..O.#O.O#O...O...O........#..OOO#..#....OO.##..O.
-.#O#OO....OO.O...O..#..#O.O..#...#..#O.O....O#..OO.....O.#....#O..#....O.....#O.#......OO#O.........
-.O..#.O#..O#.O..#.#.......#O......O.....O.#O...#O.#.#..OO.O..#..O............#OO#..#..O..#O..O.O.#..
-..#..O.......#O##O.......O.O#O....OOO..OOO..O.#..OO..#O..O#......#.###O.O.#O.#.....#..O.##.OO.#.....
-.O.#...O...O...O#..O.##O...#.OO......OOO.OO..#...#...OO#..O.#...OO#....###..O.#..#.....#....O.O.....
-....O.OO..O#........#.#.#O..#.O....O...###.....O.O.#O#....O...O...##.#O#..O.#......OO.#####..#...#O.
-O...#OO..##O##.......O#.O#...OOO.....O.O..#..OO......O#.##....#......O##......#OO#.#.........OOOO..#
-O.O..##..O..#.#O.....#.OO.O...OO..#...O...###.....O..#....#.....#..O.O..#O.O#OO....#..OO.OO#....OO..
-.OOO#..#.O.........O..O...O........OO#....#..#..##....O.....#......O....O#.O.O.OO..##...O.#....#....
-....O...##O...#...OO.#O....O..O.O.O....#...O#O#..##.O.......O#.#..OO.#...#....O.....O..O#O#..#.O....
-..#....O.O....#..O.OO.......O#..OO......O.O...OO.......O..#..O....OO#..O.O......#.......#...#....#.O
-.....O#.........#...O.O..O.O#..O....OO..O.....O....O..#O..O.O....#...#.OO..O.#.#..O.#O...#..O..#....
-#....#..##...O#OO....#..#.#O...............#.O........O..#.##............#.O#...#..##...O.....#..#.O
-#.OO...O...O##...#O#..O....O.O..OO..O.OO.#OO.O..O...O#........#O.O....#.#.OO.....O....#..#O..O.#.###
-......O....#...........#...#.........O.O....#..#...#..#.O#....#.O.#.O..#...O..#.......O.#..O#...#...
-.#O.OO#.#...#.O.#..OO....O.......O#OO.O...........#.OO..#...##.O.#....O....##O....#..#...O#..O.O..O.
-.#..##...#.O#O.O..O.......O..#...OO#......##.#O.#.O#O...##..#.O...#O.....O....O.....#......O..#.....
-#O.O.O.OO.....O#..#....O.O#.O.OO.O#..O.#O.....O#.O#.O.O.........O...#...O.O..#O.#.#O#......#OO...##.
-....#O....#.#.#O.O.OO.O.#..O.O...#...#...O....O........O#O.......#OO.#.........#....OO...O..O.......
-.#.O#O..#.O.#...O#O.#......#.....O.O.O.#OOOO..##OOO.O.O....O..O#OO......OOO..O.##...O.O.#.....O.O#O.
-##..........O...O.#...#O............#O.O......O..O#.....O......O..##.O.#O#.#......#..#....O...#..O..
-..#..#..O#..#..O#O.........O##......##O.O....##.OOO#..#........O#OO..#.#.O.OO.#.OO#O.#.O..#.......##
-.OO....O..OO...O.#..O....#O......##O.OOO..#.#O...#......O..#.#.....O#...OO.#...#O###...O...#........
-O...O..OO..#.....OOO.#..O.##....#.O..O.OOO..#...O.#O..........#...O##....O.O..O...O...O.O.#O..O#.O#.
-OO.O#O.##O.#.#..O.O.#.#..#...O..O..#...OO.OOO..O.O...OOO.#..O..O..OO.O##O#.#O...O...#...O.#O.#......
-..OO.#.##.............#..#O.......O.O.#O..O.OOOO....OO..O..........#O.#..O.##.........#.....O.......
-.O..O.##O#...O.#.O.#.......##.#...#....O#..O..OO............###.....#.O.O......##.#..O...........O.#
-.##...#OO.O.O#.....O..O.OO##O.#..O..O...OO..#..O.O..O.O.O....O..............O#.###OO.#.#.#O#....#...
-..O#...#.....O.OOO.....#O..O.#.#.....##....##O#..............O...O..#O##.#..O.....O..#...O.#O...O..O
-O..OO.O....##O##O#......O..#O.O........O##O...#...O..O.....O.##..#....OO#O..O..#.....##..OO....O..O.
-OO#.#OO.....O###....#O..O...#...#O..#O.#.O#.O.....O#O.O..O....O....##.....#....O.....#O...##....O.#.
-#.#..#O#..O.#.#OO..##...#...#..OO.#..O....O..#...OO....OO..#..#....#....#O#.O...OO...#O..........O..
-O..O.O.....O.....#.........O....O#......OO#..O..#.#.O##..#O.#..#...#..O.O......#.O..O..#.O......OO..
-.#.......#..#.O.....O.#O#.#.#.....#O####.......O.#O........###..##...##..O..##...O.OO..#..O.O......#
-.#.O.#.........#O.O.O...O..OO...O..O.O..O....O#..........O...O.O#.OO#..OOO.O..#O.O.O#O..O#.#..#OO..O
-#.#.#...#.#.....#..#........O..#..#..###..O..O...O.O#..##.OOO.O...O.#O...O.......OOO#....#.#...O.#..
-...#.O...O#O..O...#..O..O..O#.#...#..#....O.O..O...OO.#..O.OO.##O..OOOOO..#......#..OO##...OOO..O..O
-.O...O..O.#O.....OO...#OO#..O#.....O..#.O.#.O#.O.##.O.OOO#OO..#..OO#OOO.O....O.....O#O.O..O..O##.O##
-O.O.............O.......OOO#.....#.......#....#.O..##.#...#...OO......#..#..O.O...O.#.O.O..##O#..#..
-O...#...##.O.#.##.O#.#..............O.O.#......###...OO......O.#..#.#O.......O#.#.O..O.OO#..O.....O.
-OO#.....#.....#.#.#O.O#..#.#...........#...##.....#....O#...#....O#..#O.#O......O#.#.#.O..#.O##...#.
-..#....O..##..#.###.OO.#......O..................##O#O....#OO#......O..O...##.##O....OO..#..#OO##.##
-...#........O.#...O.##.#..#O..O......#....O##.#..OO..O#..#O......O.....#..O..#.OO.O.#..OO....O..#.#O
-.O#..O......O.#OOO.....#.O...OO#.#.........#...O...O.#...##O.........#O#.O.#.#O.#..#.##.....O...OO..
-.....OOOO..O.O####..#.O....#..OO......O.O.......OO#OO#O.OO..O##....#.........OO...#....O.O..O..#....
-#O...#..#...O.OO#..##O...O.#......O..O##.#...#....OO..#...#..O....#.O...O##.#O#....#...#...O.O...#.O
-..#.OOO.....O#......##.....#..O#O...#..#...........O#O.OO...#....#.........O....#..O.#...#..#..#.#O.
-...OO.#..O...O..#.#O.##O.#..O.O.#O....O.....#..#O#.O....O.OO#.O..#..#...#...O...##.#.....O......O#O.
-.###O....#......O.O..O...#...O..#......O.........O..O...##....#.#...#..........O##O...#..O..O.OO##..
-..O.#O.O#...#..O#...O..#O..#.OO.....#.O.....##.O##.##...O..#...#O.O...#......#O...O...OO.#....O#..O.
-...O.O...O.O.O.O#O.###..#.##O......O.O.O#..O...#.#O.O...##..##O.OOOO...#...O....O.##O#O.....#O#..O#O
-....OOO#.....O..O.......#O#...........#.....#.#.#.....O..OO#.....O.OO..#...####.O.#O#.....#..#....OO
-O##..OO..O#.#..O...#.....OOO......O.....O.....O#OO....O...O....O.#...#...OO...#..#....#.......O#.O.#
-..#OOOOO.O#..#....##O.O#.....O.O.......O#......O#..#......#.O##.#...#O...O........O......O.O..O.....
-..OO........O..O#..O.O.#..O.O#.O#...O.......##..O#......#O.O.#.#......O..#..#O.O.#.#.O#......##O.O..
-.#......#..#...O..OOOO#.O#..........O.....#...OOO...O...O.#O..O..O...O..O...#.O....#O#......OO..OO..
-...OO....O#O#OO...O.OOO.#...O.....#.O..O.O....#.O.#.O.#.#O.....O..#.##...OO#..#.O....#O.#.O.#O.O.#..
-..#..#..O......#.#O.O...#.OO.....OOO...###.....O...O#..##.O...##O#..#.O.#.....O..#..O.O.OOOO..O...##
-.#....O.O.O..O.O........O.#O..#OO#.##..O#..#.....OO#..O.#O..O..#.......O..#.....##....O..O.O#.......
-....O...#OOO..##O...#OO...O...O......##......O....O..O..O....OO..#O..#....#..OO##.O.#.#.#...#.OO#...
-.OO#..#...O.......O..#....OO..O#.....OO.##...##...#O......O.....#........#.##.O.....O..O..O..O.....O
-.O..O#...#..O.OO...O....##.O..##...O..##O.#....O#.O....O...O.#O....#O......OO#..........O....##OO.#.
-..OO..##.#.O.O....O....#O...O#......##......O.O.O#......O.#O......##.O.......O....OO#O.O..##O.O...O.
-#.O....#..##........O#O.OO.#.#..OOO.#.O...#.##...#.O#....#.......#..O#O....O.O...O..O....OO.O#....O.
-..#....OO..........O##.##..O..#..O.......#.........##OO....#.O...O.O##.......OO...#O#....O..##..OO..
-#O.##O#....#.OOO.......O..#.O.O..O.#.OOOO#.#.#...##.O.OO......O..OO#.#...#.O##O.O........O.#...O.#..
-#....O.OO#.#....O.#...O....#..O...#..O.OO..O..#..#..O#.#..OOO.O.O..#.O.#.#.......#.O#..O.##....OO...
-..#.....#.O...O.O...O...O.#.#.O...OO#.#.#.....O#O.OOO.O.#......O.O.....O.#..O....O.O.OOOO#...O#.##.#
-..O.O#O..OO......O......OO.#.#.....OO#...O..O......OOO..#..O...O#.##..O...#.......#.#.O....#..##.O.O
-...O..#O..O#O...#..#...##.#....O.#...#.OOO.#O.......O...O....#...O...##...O..O..#..#...O#O..#...#.O.
-..#..##.O#O...OO##..#..OO.#......O......O....#.#.O..O.......OO.#O.......#O...#.O.O.#....##O#O..OO...
-.OO...O..O...#.OOO..OO.O...#O.......#.#.....OO..O..#.OO###.O.#O........#O.O#.O...............O.....#
-....#O..#..#....O.##O.O#.O...#O..O#..#....O#.O...O....O......##...##O...#.#...#..#.O..O..#..#..O.O#O
-.....#..O#...O.#.#.#......#O.O..#.#....O....##.O##O..O.#O..O.O.....#O....#..#.#..O.#O#......OOO#...O
-.#..O...##.O.....##.#....#O.O.##..#.......O.....#.#..O##........#.O..........#.#O.#..#......#O#..#O.
-OO.#...O...O#.......OO.OOO#.O#....#.OO...O..OO....OO#O.O#....O..O....O..O.O..O....O........O.O.#....
-.O...#.......O..#OO.....#O.O...#O.#O..#.#.....#....O....#.....O..#O.#.O......O.O.OOOO.....O...#....#
-.#OO.....#.#O..O........O.#O..O..#......O.O....OO.#..OO#..O#.#O.#O.O...#......O#O....#.#O...#...#.OO"""
-
-CONVERGENCE = 1
-
-
-def move_north(coordinates: tuple, matrix: list):
-    row, col = coordinates[0], coordinates[1]
-    while True:
-        if row == 0:
-            return matrix
-        previous_val = matrix[row - 1][col]
-        if previous_val == "#" or previous_val == "O":
-            return matrix
-        if matrix[row][col] == "O" and previous_val == ".":
-            matrix[row][col], matrix[row - 1][col] = ".", "O"
-        row -= 1
-
-
-def move_south(coordinates: tuple, matrix: list):
-    row, col = coordinates[0], coordinates[1]
-    while True:
-        if row == len(matrix) - 1:
-            return matrix
-        next_val = matrix[row + 1][col]
-        if next_val == "#" or next_val == "O":
-            return matrix
-        if matrix[row][col] == "O" and next_val == ".":
-            matrix[row][col], matrix[row + 1][col] = ".", "O"
-        row += 1
-
-
-def move_east(coordinates: tuple, matrix: list):
-    row, col = coordinates[0], coordinates[1]
-    while True:
-        if col == len(matrix[0]) - 1:
-            return matrix
-        next_val = matrix[row][col + 1]
-        if next_val == "#" or next_val == "O":
-            return matrix
-        if matrix[row][col] == "O" and next_val == ".":
-            matrix[row][col], matrix[row][col + 1] = ".", "O"
-        col += 1
-
-
-def move_west(coordinates: tuple, matrix: list):
-    row, col = coordinates[0], coordinates[1]
-    while True:
-        if col == 0:
-            return matrix
-        prev_val = matrix[row][col - 1]
-        if prev_val == "#" or prev_val == "O":
-            return matrix
-        if matrix[row][col] == "O" and prev_val == ".":
-            matrix[row][col], matrix[row][col - 1] = ".", "O"
-        col -= 1
-
-
-NEWBOARD = [list(b) for b in BOARD.splitlines()]
-
-
-for row in range(len(NEWBOARD)):
-    for col in range(len(NEWBOARD[row])):
-        NEWBOARD = move_north((row, col), NEWBOARD)
-
-
-# count points
-points = 0
-lenboard = len(NEWBOARD)
-for i in range(lenboard):
-    number_of_Os = NEWBOARD[i].count("O")
-    algo_row = lenboard - i
-
-    print(number_of_Os, algo_row)
-    points += number_of_Os * algo_row
-
-# PART 2
 import time
 
-print("hello")
+class BoardManipulator:
+    def __init__(self, board_str: str):
+        self.board = [list(row) for row in board_str.splitlines()]
+        self.lenboard = len(self.board)
+    
+    def move_north(self, row: int, col: int):
+        while True:
+            if row == 0:
+                break
+            previous_val = self.board[row - 1][col]
+            if previous_val in ("#", "O"):
+                break
+            if self.board[row][col] == "O" and previous_val == ".":
+                self.board[row][col], self.board[row - 1][col] = ".", "O"
+            row -= 1
+    
+    def move_south(self, row: int, col: int):
+        while True:
+            if row == self.lenboard - 1:
+                break
+            next_val = self.board[row + 1][col]
+            if next_val in ("#", "O"):
+                break
+            if self.board[row][col] == "O" and next_val == ".":
+                self.board[row][col], self.board[row + 1][col] = ".", "O"
+            row += 1
 
-NEWBOARD = [list(b) for b in BOARD.splitlines()]
+    def move_east(self, row: int, col: int):
+        while True:
+            if col == len(self.board[0]) - 1:
+                break
+            next_val = self.board[row][col + 1]
+            if next_val in ("#", "O"):
+                break
+            if self.board[row][col] == "O" and next_val == ".":
+                self.board[row][col], self.board[row][col + 1] = ".", "O"
+            col += 1
 
+    def move_west(self, row: int, col: int):
+        while True:
+            if col == 0:
+                break
+            prev_val = self.board[row][col - 1]
+            if prev_val in ("#", "O"):
+                break
+            if self.board[row][col] == "O" and prev_val == ".":
+                self.board[row][col], self.board[row][col - 1] = ".", "O"
+            col -= 1
 
-def spinning_cycle(board: list) -> list:
-    # north
-    start = time.time()
+    def process_north(self):
+        for row in range(self.lenboard):
+            for col in range(len(self.board[row])):
+                self.move_north(row, col)
+    
+    def spinning_cycle(self):
+        start = time.time()
 
-    for row in range(len(board)):
-        for col in range(len(board[row])):
-            board = move_north((row, col), board)
-    # west
-    for row in range(len(board)):
-        for col in range(len(board[row])):
-            board = move_west((row, col), board)
-    # south
-    for row in reversed(range(len(board))):
-        for col in range(len(board[row])):
-            board = move_south((row, col), board)
-    # east
-    for row in range(len(board)):
-        for col in reversed(range(len(board[row]))):
-            board = move_east((row, col), board)
-    end = time.time()
-    # print(end - start)
-    return board
+        # Move north
+        for row in range(self.lenboard):
+            for col in range(len(self.board[row])):
+                self.move_north(row, col)
+        
+        # Move west
+        for row in range(self.lenboard):
+            for col in range(len(self.board[row])):
+                self.move_west(row, col)
+        
+        # Move south
+        for row in reversed(range(self.lenboard)):
+            for col in range(len(self.board[row])):
+                self.move_south(row, col)
+        
+        # Move east
+        for row in range(self.lenboard):
+            for col in reversed(range(len(self.board[row]))):
+                self.move_east(row, col)
+        
+        end = time.time()
+        print(f"Cycle time: {end - start} seconds")
 
+    def calculate_points(self) -> int:
+        points = 0
+        for i in range(self.lenboard):
+            number_of_Os = self.board[i].count("O")
+            algo_row = self.lenboard - i
+            points += number_of_Os * algo_row
+            print(number_of_Os, algo_row)
+        return points
 
-comparetest = [list(b) for b in BOARD.splitlines()]
-for i in range(10):
-    # print(i)
-    NEWBOARD = spinning_cycle(NEWBOARD)
-    # if NEWBOARD == comparetest:
-    #     print(i)
+def main():
+    BOARD = """\
+.....
+.OO..
+.#O#.
+.....
+....."""
+    
+    # PART 1
+    manipulator = BoardManipulator(BOARD)
+    manipulator.process_north()
+    points = manipulator.calculate_points()
+    print(f"Total Points (Part 1): {points}")
 
-# count points
-points = 0
-lenboard = len(NEWBOARD)
-for i in range(lenboard):
-    number_of_Os = NEWBOARD[i].count("O")
-    algo_row = lenboard - i
+    # PART 2
+    print("Part 2")
+    manipulator = BoardManipulator(BOARD)
+    for i in range(10):
+        manipulator.spinning_cycle()
+    points = manipulator.calculate_points()
+    print(f"Total Points (Part 2): {points}")
 
-    print(number_of_Os, algo_row)
-    points += number_of_Os * algo_row
+if __name__ == "__main__":
+    main()
--- day3_advent.py
+++ day3_advent.py
@@ -7,8 +7,96 @@ Created on Mon Dec 25 22:55:40 2023
 """
 from string import punctuation
 
+class MatrixAnalyzer:
+    def __init__(self, matrix_input: str):
+        self.SIGNS = list(set(punctuation))
+        self.SIGNS.remove(".")
+        self.NUMBERS = [str(i) for i in range(10)]
+        self.matrix = [list(line) for line in matrix_input.splitlines()]
 
-INPUT = """467..114..
+    def check_from_right_with_coords(self, coordinates: tuple) -> tuple:
+        row, col = coordinates
+        ret = ""
+        coords = []
+        while True:
+            if self.matrix[row][col] not in self.NUMBERS:
+                return ret, coords
+            ret += self.matrix[row][col]
+            coords.append((row, col))
+            if col == len(self.matrix[0]) - 1:
+                return ret, coords
+            col += 1
+
+    def find_all_numbers(self) -> dict:
+        signs_coords = {}
+        rows, cols = len(self.matrix), len(self.matrix[0])
+
+        for row in range(rows):
+            col = 0
+            while col < cols:
+                if self.matrix[row][col] in self.NUMBERS:
+                    number, coords = self.check_from_right_with_coords((row, col))
+                    signs_coords[(row, col)] = {"Number": number, "Coordinates": coords}
+                    col += len(number) - 1
+                col += 1
+
+        return signs_coords
+
+    def neighbour_coordinates(self, coord: tuple) -> list:
+        x, y = coord
+        max_left = 0
+        max_right = len(self.matrix[0]) - 1
+        max_up = 0
+        max_down = len(self.matrix) - 1
+
+        neighbours = []
+        for row in range(x - 1, x + 2):
+            for col in range(y - 1, y + 2):
+                if max_down >= row >= max_up and max_right >= col >= max_left:
+                    neighbours.append((row, col))
+        return neighbours
+
+    def calculate_part1(self) -> int:
+        allowed_numbers = []
+        all_nums = self.find_all_numbers()
+        for coord, data in all_nums.items():
+            coords_temp = data["Coordinates"]
+            neighbour_coords_temp = []
+            for j in coords_temp:
+                neighbour_coords_temp += self.neighbour_coordinates(j)
+            for j in neighbour_coords_temp:
+                if self.matrix[j[0]][j[1]] in self.SIGNS:
+                    allowed_numbers.append(int(data["Number"]))
+                    break
+
+        return sum(allowed_numbers)
+
+    def calculate_part2(self) -> int:
+        enginecoords_numbers = {}
+        all_nums = self.find_all_numbers()
+        for coord, data in all_nums.items():
+            coords_temp = data["Coordinates"]
+            neighbour_coords_temp = []
+            for j in coords_temp:
+                neighbour_coords_temp += self.neighbour_coordinates(j)
+            neighbour_coords_temp = list(set(neighbour_coords_temp))
+            for j in neighbour_coords_temp:
+                if self.matrix[j[0]][j[1]] == "*":
+                    if j not in enginecoords_numbers:
+                        enginecoords_numbers[j] = [data["Number"]]
+                    else:
+                        enginecoords_numbers[j].append(data["Number"])
+                    break
+
+        number = 0
+        for nums in enginecoords_numbers.values():
+            if len(nums) == 2:
+                number += int(nums[0]) * int(nums[1])
+
+        return number
+
+def main():
+    INPUT = """467..114..
 ...*......
 ..35..633.
 ......#...
@@ -18,188 +106,16 @@ INPUT = """467..114..
 ......755.
 ...$.*....
 .664.598.."""
+    
+    analyzer = MatrixAnalyzer(INPUT)
+    
+    # Part 1
+    result_part1 = analyzer.calculate_part1()
+    print(f"Result of part 1 is {result_part1}")
+    
+    # Part 2
+    result_part2 = analyzer.calculate_part2()
+    print(f"Result of part 2 is {result_part2}")
 
-SIGNS = list(set(punctuation))
-SIGNS.remove(".")
-NUMBERS = [i for i in "1234567890"]
-
-INPUT = [list(l) for l in INPUT.splitlines()]
-
-
-def check_from_right_with_coords(coordinates: tuple, matrix: list) -> str:
-    row, col = coordinates[0], coordinates[1]
-    ret = ""
-    coords = []
-    while True:
-        if matrix[row][col] not in NUMBERS:
-            return ret, coords
-        ret = ret + matrix[row][col]
-        coords.append((row, col))
-        if col == len(matrix[0]) - 1:
-            return ret, coords
-        col += 1
-
-
-def find_all_numbers(matrix: list) -> dict:
-    signs_coords = {}
-    rows, cols = len(matrix), len(matrix[0])
-
-    for row in range(rows):
-        col = 0
-        while col < cols:
-            if matrix[row][col] in NUMBERS:
-                number, coords = check_from_right_with_coords((row, col), matrix)
-                signs_coords[(row, col)] = {"Number": number, "Coordinates": coords}
-                col += len(number) - 1
-            col += 1
-
-    return signs_coords
-
-
-def neigbour_coordinates(coord: tuple, matrix: list) -> bool:
-    x, y = coord[0], coord[1]
-    max_left = 0
-    max_right = len(matrix[0]) - 1
-    max_up = 0
-    max_down = len(matrix) - 1
-
-    neighbours = []
-    for row in range(x - 1, x + 1 + 1):
-        for col in range(y - 1, y + 1 + 1):
-            if max_down >= row >= max_up and max_right >= col >= max_left:
-                neighbours.append([row, col])
-    return neighbours
-
-
-allowed_numbers = []
-allnums = find_all_numbers(INPUT)
-for i in allnums.keys():
-    coords_temp = allnums[i]["Coordinates"]
-    neighbour_coords_temp = []
-    for j in coords_temp:
-        neighbour_coords_temp += neigbour_coordinates(j, INPUT)
-    for j in neighbour_coords_temp:
-        if INPUT[j[0]][j[1]] in SIGNS:
-            allowed_numbers.append(int(allnums[i]["Number"]))
-            break
-
-print(f"Result is {sum(allowed_numbers)}")
-
-
-enginecoords_numbers = {}
-allnums = find_all_numbers(INPUT)
-for i in allnums.keys():
-    coords_temp = allnums[i]["Coordinates"]
-    neighbour_coords_temp = []
-    for j in coords_temp:
-        neighbour_coords_temp += neigbour_coordinates(j, INPUT)
-    neighbour_coords_temp = list(set([(i[0], i[1]) for i in neighbour_coords_temp]))
-    for j in neighbour_coords_temp:
-        if INPUT[j[0]][j[1]] == "*":
-            if j not in enginecoords_numbers:
-                enginecoords_numbers[j] = [allnums[i]["Number"]]
-            else:
-                enginecoords_numbers[j].append(allnums[i]["Number"])
-            break
-
-number = 0
-for i in enginecoords_numbers.keys():
-    if len(enginecoords_numbers[i]) == 2:
-        temp_num = int(enginecoords_numbers[i][0]) * int(enginecoords_numbers[i][1])
-        number += temp_num
-
-print(f"Results of part 2 are {number}.")
-
-
-# PREVIOUS ATTEMPT
-
-
-# def find_all_signs(matrix: list)->list:
-#     signs_coords = []
-#     rows, cols = len(matrix), len(matrix[0])
-#     for row in range(rows):
-#         for col in range(cols):
-#             if matrix[row][col] in SIGNS:
-#                 signs_coords.append((row,col))
-#     return signs_coords
-
-# def check_from_left(coordinates: tuple,
-#                     matrix: list)->str:
-#     row,col = coordinates[0], coordinates[1]
-#     ret = ""
-#     while True:
-#         if matrix[row][col] == ".":
-#             return ret
-#         ret = matrix[row][col] + ret
-#         if col == 0:
-#             return ret
-#         col -= 1
-
-# def check_from_right(coordinates: tuple,
-#                     matrix: list)->str:
-#     row,col = coordinates[0], coordinates[1]
-#     ret = ""
-#     while True:
-#         if matrix[row][col] == ".":
-#             return ret
-#         ret = ret  + matrix[row][col]
-#         if col == len(matrix[0]) - 1:
-#             return ret
-#         col += 1
-
-# def check_from_both_sides(coordinates: tuple,
-#                     matrix: list)->str:
-#     left = check_from_left(coordinates, matrix)
-#     right = check_from_right(coordinates, matrix)
-#     return left + right[1:]
-
-# def get_list_of_numbers_from_coordinate(coordinates: tuple,
-#                     matrix: list):
-
-#     row,col = coordinates[0], coordinates[1]
-#     list_of_coeffs = []
-
-#     if col != 0:
-#         if matrix[row][col-1] != ".":
-#             list_of_coeffs.append(check_from_left((row,col-1), matrix))
-
-#     if col != len(matrix[0])-1:
-#         if matrix[row][col+1] != ".":
-#             list_of_coeffs.append(check_from_right((row,col+1), matrix))
-
-#     """
-#     First check middles, if letter then can do left and right at once
-#     (for middles, check if the row is not first/last)
-
-#     If not middle, check left and right adjestents,
-#     (also check row first/last)
-#     """
-
-#     if row != 0:
-#         if matrix[row-1][col] != ".":
-#             list_of_coeffs.append(check_from_both_sides((row-1,col), matrix))
-#         else:
-#             if matrix[row-1][col-1] != ".":
-#                 list_of_coeffs.append(check_from_left((row-1,col-1), matrix))
-#             if matrix[row-1][col+1] != ".":
-#                 list_of_coeffs.append(check_from_right((row-1,col+1), matrix))
-
-
-#     if row <= len(matrix):
-#         if matrix[row+1][col] != ".":
-#             list_of_coeffs.append(check_from_both_sides((row+1,col), matrix))
-#         else:
-#             if matrix[row+1][col-1] != ".":
-#                 list_of_coeffs.append(check_from_left((row-1,col-1), matrix))
-#             if matrix[row+1][col+1] != ".":
-#                 list_of_coeffs.append(check_from_right((row-1,col+1), matrix))
-#     return list_of_coeffs
-
-
-# # coefs = find_all_signs(INPUT)
-# # numbers = []
-# # for i in coefs:
-# #     numbers += get_list_of_numbers_from_coordinate(i, INPUT)
-
-
-# gg= find_all_numbers(INPUT)
+if __name__ == "__main__":
+    main()
